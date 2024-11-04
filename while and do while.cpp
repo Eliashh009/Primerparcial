82c33ff9e70bcdc163994335722cff384c74b5ef
Elias Hernández <elias.hernandezh@my.unitec.edu.mx>
2024-11-04 11:42:33 -0600

    While and do while.cpp

---
+++ while and do while.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <limits>
+#include <iomanip>
+#include <locale>
+using namespace std;
+
+int main()
+{
+    int i = 0, suma = 0;
+        while (i <= 9)
+        {
+            cout << i << 0, 9;
+            suma = suma + i;
+            i++;
+    }
+    cout << endl;
+    cout << "total: " << suma << endl;
